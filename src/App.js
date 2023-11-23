c750c82911e7c55ee8a5aa78ce6e8ac948bf2b8c
Hülya Nergiz <akin.hulya@outlook.com>
2023-11-24 00:15:21 +0300

    some imports and fetch-get axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
 import Item from "./components/Item";
 import FavItem from "./components/FavItem";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAnother } from "./actions";
 
 export default function App() {
-  const loading = false;
-  const current = null;
-  const favs = [];
-
+  const loading = useSelector((depo) => depo.loading);
+  const current = useSelector((depo) => depo.current);
+  const favs = useSelector((depo) => depo.favs);
+  const dispatch = useDispatch();
   function addToFavs() {
   }
 
+  }
+
+  const fetchAnotherHandler = () => {
+    dispatch(fetchAnother());
+  };
+  useEffect(() => {
+    fetchAnotherHandler();
+  }, []);
 
   return (
     <div className="wrapper max-w-xl mx-auto px-4">
@@ -34,11 +44,14 @@ export default function App() {
 
       <Switch>
         <Route exact path="/">
-          {loading && <div className="bg-white p-6 text-center shadow-md">YÜKLENİYOR</div>}
+          {loading && (
+            <div className="bg-white p-6 text-center shadow-md">YÜKLENİYOR</div>
+          )}
           {current && <Item data={current} />}
 
           <div className="flex gap-3 justify-end py-3">
             <button
+              onClick={fetchAnotherHandler}
               className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
             >
               Başka bir tane
